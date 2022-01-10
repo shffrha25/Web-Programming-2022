997fb8a74d0d7f4ebebe789ff9b9eaec76d9c230
shofa farhatul muslimah <91241574+shffrha25@users.noreply.github.com>
2022-01-11 00:25:23 +0700

    Add files via upload

---
+++ Tugas Praktikum 7/script.js
@@ -0,0 +1,13 @@
+console.log('hallo, Harfa')
+
+// belajar variable
+// statis
+const phi = 3.14;
+// phi = 3.15; error
+console.log (phi);
+
+
+// Dinamis
+let age = 19;
+age = 20;
+console.log(age);
